c433ccebf1c27534bed1475174fe8f8edd8aa10c
Alex MacArthur <alex@macarthur.me>
2020-11-12 21:42:49 -0600

    Bump version.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '6cfffa74ecad5d10ad9011e27503416d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '4515ec644cca855be5e8287da969af73');
